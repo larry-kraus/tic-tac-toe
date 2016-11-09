633c4b3a1a82e88f35ec67a97cd49b93d4bf12c8
larry-kraus <krauslarry@gmail.com>
2016-11-09 16:56:27 -0700

    can turn squares blue

--- script.js
+++ script.js
@@ -1,12 +1,60 @@
-console.log("JS linked!");
+var micCheck = function() {
+	console.log("hell yeah");
+};
 
-var micCheck = console.log("hell yeah");
+var xTurn;
+var oTurn;
+var turnArray = ["x", "o", "x", "o", "x", "o", "x", "o", "x", "o", "x", "o"];
 
-var bigDiv = document.getElementsByClassName('bigDiv');
+if (turnArray[i] = "x") {
+	return xTurn === true && oTurn === false;
+}
+else {
+	return xTurn === false && oTurn === true;
+}
 
-var square1 = document.getElementsByClassName('square1');
+var whosTurn = function() {
+	xTurn = true;
+}
 
-//square1.addEventListener("click", 'micCheck');
+
+var squarePick = function() {
+	this.setAttribute("class", "xSquare");
+}
+//make an if else statement here
+
+var bigDiv = document.getElementById('bigDiv');
+
+var square1 = document.getElementById('square1');
+square1.addEventListener("click", squarePick);
+
+var square2 = document.getElementById('square2');
+square2.addEventListener("click", squarePick);
+
+var square3 = document.getElementById('square3');
+square3.addEventListener("click", squarePick);
+
+var square4 = document.getElementById('square4');
+square4.addEventListener("click", squarePick);
+
+var square5 = document.getElementById('square5');
+square5.addEventListener("click", squarePick);
+
+var square6 = document.getElementById('square6');
+square6.addEventListener("click", squarePick);
+
+var square7 = document.getElementById('square7');
+square7.addEventListener("click", squarePick);
+
+var square8 = document.getElementById('square8');
+square8.addEventListener("click", squarePick);
+
+var square9 = document.getElementById('square9');
+square9.addEventListener("click", squarePick);
+
+//bigDiv.appendChild(square1);
+
 
 	//textContent = "Awwww yeahhhh";
-//bigDiv.appendChild(square1);
+//
+micCheck();
