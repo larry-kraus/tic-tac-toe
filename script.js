2238d6ac5c700c56a492af2b8404cc12dc52240f
larry-kraus <krauslarry@gmail.com>
2016-11-09 15:49:33 -0700

    html completed

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log("JS linked!");
+
+var micCheck = console.log("hell yeah");
+
+var bigDiv = document.getElementsByClassName('bigDiv');
+
+var square1 = document.getElementsByClassName('square1');
+
+//square1.addEventListener("click", 'micCheck');
+
+	//textContent = "Awwww yeahhhh";
+//bigDiv.appendChild(square1);
